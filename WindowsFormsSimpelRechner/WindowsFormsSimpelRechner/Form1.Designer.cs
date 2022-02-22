5390f1829f75da82252180e3837553f6b2269016
ItzCelyrian <ItzCelyrian@gmail.com>
2022-02-22 08:38:15 +0100

    Release v1.0

---
+++ WindowsFormsSimpelRechner/WindowsFormsSimpelRechner/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsSimpelRechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_equals = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_end = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(12, 12);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(100, 100);
+            this.button_add.TabIndex = 0;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_equals
+            // 
+            this.button_equals.Location = new System.Drawing.Point(12, 118);
+            this.button_equals.Name = "button_equals";
+            this.button_equals.Size = new System.Drawing.Size(100, 100);
+            this.button_equals.TabIndex = 1;
+            this.button_equals.Text = "=";
+            this.button_equals.UseVisualStyleBackColor = true;
+            this.button_equals.Click += new System.EventHandler(this.button_equals_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(12, 224);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(100, 100);
+            this.button_clear.TabIndex = 3;
+            this.button_clear.Text = "Clear";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(396, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 263);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(290, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(315, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Zahl 2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Zahl 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ausgabe";
+            // 
+            // button_end
+            // 
+            this.button_end.Location = new System.Drawing.Point(414, 225);
+            this.button_end.Name = "button_end";
+            this.button_end.Size = new System.Drawing.Size(100, 100);
+            this.button_end.TabIndex = 9;
+            this.button_end.Text = "Beenden";
+            this.button_end.UseVisualStyleBackColor = true;
+            this.button_end.Click += new System.EventHandler(this.button_end_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 337);
+            this.Controls.Add(this.button_end);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_equals);
+            this.Controls.Add(this.button_add);
+            this.Name = "Form1";
+            this.Text = "Simpler Rechner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_equals;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_end;
+    }
+}
+
---
+++ WindowsFormsSimpelRechner/WindowsFormsSimpelRechner/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsSimpelRechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_equals.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label1.Visible = false;
+            label3.Visible = false;
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            button_equals.Visible = true;
+            textBox2.Visible = true;
+            label1.Visible = true;
+        }
+
+        private void button_equals_Click(object sender, EventArgs e)
+        {
+            textBox3.Visible = true;
+            label3.Visible = true;
+            double zahl1 = Convert.ToDouble(textBox1.Text);
+            double zahl2 = Convert.ToDouble(textBox2.Text);
+            string output = Convert.ToString(zahl1 + zahl2);
+            textBox3.Text = output;
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            button_equals.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            label1.Visible = false;
+            label3.Visible = false;
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
